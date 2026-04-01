55cceab420e09b5dd8d629fbcbb8b4a70feb4b29
ZhopaPenis <zhopapenis9@gmail.com>
2026-04-01 22:30:32 +0300

    added update for settings view

--- TopBarMenu/View/SettingsView.swift
+++ TopBarMenu/View/SettingsView.swift
@@ -1,18 +1,63 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State var isShowRGB: Bool = false // use the core data for reminding the state
+    
     var body: some View {
         VStack {
-            Text("Settings of app")
-                .font(.title)
-                .padding()
             
-            Divider()
+            HStack {
+                Text("TopBarMenu")
+                    .font(.largeTitle)
+            }
+            .padding(.top, 40)
             
-            Text("Здесь можно будет добавить настройки")
+            Spacer()
+            
+            VStack {
+                HStack {
+                    Text("Show RGB colors in tab")
+                    
+                    Spacer()
+                    
+                    Toggle("", isOn: $isShowRGB)
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("Frequency of data updates")
+                    
+                    Spacer()
+                    
+                    TimePeacker()
+                }
+                .padding(.horizontal)
+            }
+            
+            Spacer()
             Spacer()
         }
         .frame(width: 300, height: 500)
+        .navigationTitle("Settings")
+    }
+}
+
+struct TimePeacker: View {
+    @State private var selected = ""
+    
+    let options = ["1 sec", "5 sec", "10 sec", "15 sec", "20 sec"]
+    
+    init() {
+        self.selected = options[0]
+    }
+    
+    var body: some View {
+        Picker("", selection: $selected) {
+            ForEach(options, id: \.self) { option in
+                Text(option)
+            }
+        }
+        .pickerStyle(.menu)
     }
 }
 
