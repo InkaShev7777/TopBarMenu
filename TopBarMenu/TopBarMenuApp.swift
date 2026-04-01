cae0095520ed3dcc5388df047f383088f481a2d6
ZhopaPenis <zhopapenis9@gmail.com>
2026-04-01 12:35:02 +0300

    Initial Commit

---
+++ TopBarMenu/TopBarMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopBarMenuApp.swift
+//  TopBarMenu
+//
+//  Created by Ilya Schevchenko on 01.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct TopBarMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
